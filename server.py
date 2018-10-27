8487bd1e25514910eb4823479c14d5430324aecb
XALM1337X <loganmitchell2011@gmail.com>
2018-10-27 10:24:02 -0400

    Correct server....

--- server.py
+++ server.py
@@ -8,35 +8,24 @@ class ThreadedServer(object):
 		self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 		self.sock.bind((self.host, self.port))
 	def listen(self):
-		self.sock.listen(5)
+		self.sock.listen(2)
 		while True:
 			client, address = self.sock.accept()
 			client.settimeout(10)
 			threading.Thread(target = self.listenToClient,args = (client,address)).start()
 	def listenToClient(self, client, address):
-		size = 8192
-		socfile = client.makefile('rw', size)
+		size = 4096
 		while True:
-			data = socfile.readline().strip()
-			request = data.split(" ")
-			if data:
-				try:
-					file = open(request[1][1:len(request[1])])
-					data = file.read()
-					socfile.write('HTTP/1.1 200 OK\n\n')
-					socfile.write('<html><head></head>')
-					socfile.write('<body>')
-					socfile.write('<h1>%s</h1>' %(data))
-					socfile.write('</body>')
-					socfile.write('</html>')
-					socfile.flush()
-					socfile.close()
-					client.shutdown(SHUT_WR)
-					client.close()
-				except:
-					socfile.write('HTTP/1.1 404 Not OK\n\n')
-					socfile.flush()
-					socfile.close()
-			else:
-				raise error('Client disconnected')
-				client.close()
+                    data = client.recv(size)
+                    strdata = data.decode('utf-8')
+                    if data:
+                        try:
+                            client.send(strdata.encode('utf-8'))
+                            client.close()
+                        except:
+                            client.send("HTTP/1.1 404 Not OK\n\n").encode('utf-8')
+                            client.close()
+                    else:
+                        raise error('Client disconnected')
+                        client.close()
+                    break
