6f6ea4fd00d11fc1f883dbe7c6ae7ab9d86031ff
U-MCS\alm2kf <alm2kf@cosc08.MCS.internal>
2018-11-02 16:24:32 -0400

    stuff.

---
+++ test.py
@@ -0,0 +1,14 @@
+from server import ThreadedServer
+
+def main():
+    #server = ThreadedServer("127.0.0.1", 1337)
+    #server.listen()
+    server = ThreadedServer("127.0.0.1", 1337).listen()
+
+
+        
+
+
+
+if __name__ == "__main__":
+    main()
