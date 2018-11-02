732b5ed135a799dc75328d60818174c2ec695795
MikeGlaw <MikeGlaw@users.noreply.github.com>
2018-11-02 19:55:45 -0400

    didn't create object

--- server.py
+++ server.py
@@ -8,20 +8,21 @@ class ThreadedServer(object):
 		self.sock = socket(AF_INET, SOCK_STREAM)
 		self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 		self.sock.bind((self.host, self.port))
+		self.sog = StateOfGame()
 	def listen(self):
 		self.sock.listen(2)
 		while True:
 			client, address = self.sock.accept()
 			client.settimeout(10)
 			hostname,hostport = client.getpeername()
-			StateOfGame.playerIPs.append(hostname)
+			self.sog.playerIPs.append(hostname)
 			threading.Thread(target = self.listenToClient,args = (client,address)).start()
 	def listenToClient(self, client, address):
 		size = 4096
 		while True:
 			data = client.recv(size)
 			strdata = data.decode()
-			strdata = strdata + hostname + StateOfGame.playerIPs
+			strdata = strdata + hostname + self.sog.playerIPs
 			if data:
 				try:
 					client.send(strdata.encode())
