8f6c1820493bc2851071cf79f06e7da32c599236
Logan <logan@atomicorp.com>
2018-11-03 03:26:09 -0400

    almost working. Fixed a lot of indentation errors and wrong key names because ofbad idle usage.

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ import socket
 Game = GameBoard("X", "orange", "O", "green")
 
 #Define constants to connect to server.
-HOST = "143.60.76.32"
+HOST = "127.0.0.1"#"143.60.76.32"
 PORT = 61001
 
 #Define the client socket.
@@ -17,66 +17,75 @@ ClientSocket.connect((HOST, PORT))
 
 #def read_packet: Function reads in one packet sent from the server.
 def read_packet():
-
-    receivedData = ""
-    byte = ""
-
-    #Iterate byte by byte to determine if we have reached the delimiter.
-    while byte != "?":
-        receivedData = receivedData + byte
-        byte = ClientSocket.recv(1).decode()
-
+    while(1):
+        receivedData = ClientSocket.recv(1024).decode()
+        if not receivedData:break
+        return receivedData
     #return our data from server.
-    return receivedData
 
-
+#def read_packet():    NEITHER OF THESE READ_PACKETS WORK FOR SOME REASON. THIS STUFF ALMOST WORKS.
+#
+#    receivedData = ""
+#    byte = ""
+#
+#    #Iterate byte by byte to determine if we have reached the delimiter.
+#    while byte != "?":
+#        receivedData = receivedData + byte
+#        byte = ClientSocket.recv(1).decode()
+#
+#    #return our data from server.
+#    return receivedData
+#
+#
 #Function will sequentially play the game.
 def playGame():
-	
+
 	incomingData = read_packet()
-	
-	while "winner" is not in incomingData:
-		
-		if "opponent_turn" is in incomingData:
+
+	while "winner" is not incomingData:
+
+		if "opponent_turn" is incomingData:
 			Game.ClearPlayerMessage()
 			opponentMove = incomingData.split(":")[1].split(",")
 			Game.gameWindow.UpdateBoard(int(opponentMove[0]), int(opponentMove[1]))
 			Game.playerTurn = True
-            
-         if Game.playerTurn:
-			Game.ClearPlayerMessage()
-			Game.SetPlayerMessage("IT'S YOUR TURN!")
-			
+
+			if Game.playerTurn:
+				Game.ClearPlayerMessage()
+				Game.SetPlayerMessage("IT'S YOUR TURN!")
+
 			while Game.playerRow != 0 and Game.playerColumn != 0:
-                outgoing = str(Game.playerRow)+","+str(Game.playerColumn)
+				outgoing = str(Game.playerRow)+","+str(Game.playerColumn)
 				ClientSocket.Send(outgoing.encode())
 				Game.SetPlayerRowColumn()
-				
+
 		incomingData = read_packet()
-				
-	if "1" is in incomingData:
-        typeOfWin = incomingData.split(":")[1].split(",")[1]
-        Game.ClearPlayerMessage()
-        Game.SetPlayerMessage("YOU WIN!")
-        DrawWinner(typeOfWin)
-    elif "0" is in incomingData:
-        typeOfWin = incomingData.split(":")[1].split(",")[1]
-        Game.ClearPlayerMessage()
-        Game.SetPlayerMessage("YOU LOSE!")
-        DrawWinner(typeOfWin)
-        
-        
+
+	if "1" is incomingData:
+		typeOfWin = incomingData.split(":")[1].split(",")[1]
+		Game.ClearPlayerMessage()
+		Game.SetPlayerMessage("YOU WIN!")
+		DrawWinner(typeOfWin)
+	elif "0" is incomingData:
+		typeOfWin = incomingData.split(":")[1].split(",")[1]
+		Game.ClearPlayerMessage()
+		Game.SetPlayerMessage("YOU LOSE!")
+		DrawWinner(typeOfWin)
+
+
+
 #def init_session: Function that intializes socket connection to server.
 def init_session():
 
-	#Init game variables. 
-    Game.playerToken = read_packet()
-    Game.playerColor = read_packet()
-    Game.opponentToken = read_packet()
-    Game.opponentColor = read_packet()
-	
-	#Notify the player that they are connected to the game. 
-    Game.SetPlayerMessage("CONNECTED!")
+	#Init game variables.
+	ClientSocket.send("init:,".encode())
+	Game.playerToken = read_packet().split(":")[0]
+	Game.playerColor = read_packet().split(":")[0]
+	Game.opponentToken = read_packet().split(":")[0]
+	Game.opponentColor = read_packet().split(":")[0]
+
+	#Notify the player that they are connected to the game.
+	Game.SetPlayerMessage("CONNECTED!")
 
 
 
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from socket import *
 import threading
 from state import StateOfGame
+import time
 class ThreadedServer(object):
 	def __init__(self, host, port):
 		self.host = host
@@ -9,30 +10,49 @@ class ThreadedServer(object):
 		self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 		self.sock.bind((self.host, self.port))
 		self.sog = StateOfGame()
-		self.hostname =""
+
 	def listen(self):
 		self.sock.listen(2)
 		while True:
 			client, address = self.sock.accept()
-			client.settimeout(10)
-			self.hostname,hostport = client.getpeername()
-			self.sog.playerIPs.append(self.hostname)
+			print("trigger1")
+			client.settimeout(9999)
+			tmphost,tmpport = client.getpeername()
+			self.sog.playerIPs.append(tmphost)
 			threading.Thread(target = self.listenToClient,args = (client,address)).start()
 	def listenToClient(self, client, address):
-		size = 4096
+		size = 1024
 		while True:
 			data = client.recv(size)
-			strdata = data.decode()
-
+			print("trigger2")
+			#strdata = data.decode()
 			if data:
+				print("trigger3")
+				client.settimeout(9999)
 				try:
-					strdata = strdata + self.hostname +" "+self.sog.playerIPs[0]
-					client.send(strdata.encode())
-					client.close()
+					print("trigger4")
+					client_data = data.decode()
+					msg = client_data.split(":")
+					host,port = client.getpeername()
+					if "init" in msg:
+						print("trigger5")
+						if len(self.sog.playerIPs) <=1:
+							print("trigger6")
+							client.send("X:,".encode())
+							client.send("magenta:,".encode())
+							client.send("O:,".encode())
+							client.send("orange:,".encode())
+						else:
+							print("trigger7")
+							client.send("O:,".encode())
+							client.send("orange:,".encode())
+							client.send("X:,".encode())
+							client.send("magenta:,".encode())
 				except:
 					client.send("HTTP/1.1 404 Not OK\n\n").encode()
 					client.close()
 			else:
-				raise error('Client disconnected')
-				client.close()
-			break
+				#raise error('Client disconnected')
+				#client.close()
+				#break
+				time.sleep(.05)
