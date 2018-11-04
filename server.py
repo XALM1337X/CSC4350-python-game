74b6b0db15fc091b0c47dd166631d0eb5480b7af
hcw2016 <woodscody2016@gmail.com>
2018-11-04 17:27:50 -0500

    Update server.py
    Server for testing. Not final implementation

--- server.py
+++ server.py
@@ -1,33 +1,58 @@
-from socket import *
-import threading
-class ThreadedServer(object):
-	def __init__(self, host, port):
-		self.host = host
-		self.port = port
-		self.sock = socket(AF_INET, SOCK_STREAM)
-		self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-		self.sock.bind((self.host, self.port))
-	def listen(self):
-		self.sock.listen(2)
-		while True:
-			client, address = self.sock.accept()
-			client.settimeout(10)
-			threading.Thread(target = self.listenToClient,args = (client,address)).start()
-	def listenToClient(self, client, address):
-		size = 4096
-		while True:
-			data = client.recv(size)
-			strdata = data.decode()
-			hostname,hostport = client.getpeername()
-			strdata = strdata + hostname
-			if data:
-				try:
-					client.send(strdata.encode())
-					client.close()
-				except:
-					client.send("HTTP/1.1 404 Not OK\n\n").encode()
-					client.close()
-			else:
-				raise error('Client disconnected')
-				client.close()
-			break
+
+import socket
+
+HOST = "127.0.0.1"
+PORT = 65531
+OUTGOING_DATA = ""
+INCOMING_DATA = ""
+
+#GAME CONSTANTS
+WINNING_PAIRS = [[(1,1), (1,2), (1,3)], [(2,1),(2,2),(2,3)]]
+PLAYER_ONE = True
+PLAYER_TWO = False
+
+IS_WINNER = False
+
+ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ServerSocket.bind((HOST, PORT))
+
+ServerSocket.listen(1)
+
+def playerOneRead():
+    receivedData = ""
+    byte = ""
+
+    while byte != "?":
+        receivedData = receivedData + byte
+        byte = playerOneConnection.recv(1).decode()
+
+    return receivedData
+
+
+while True:
+
+    playerOneConnection, playerOneAddress = ServerSocket.accept()
+    OUTGOING_DATA="player_two?"
+    playerOneConnection.send(OUTGOING_DATA.encode())
+    
+    #playerTwoConnection, playerTwoAddress = ServerSocket.accept()
+    #OUTGOING_DATA="player_two?"
+    #playerTwoConnection.send(OUTGOING_DATA.encode())
+    PLAYER_ONE=False
+    w = False
+    while not w:
+        
+        if PLAYER_ONE:
+            print("INSIDE OF IF")
+            INCOMING_DATA = playerOneConnection.recv(3).decode()
+            print("INCOMING: ", INCOMING_DATA)
+            PLAYER_ONE=False
+            w = True
+        else:
+            print("ELSE!")
+            INCOMING_DATA = "1,1"
+            OUTGOING_DATA = INCOMING_DATA+"?"
+            playerOneConnection.send(OUTGOING_DATA.encode())
+            PLAYER_ONE=True
+    
+    
